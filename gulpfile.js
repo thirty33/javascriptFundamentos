f7c70c75b6d539a67d4c2bb198b27fa0033d985e
thirty33 <joelsuarez.1101@gmail.com>
2017-08-16 18:38:50 -0500

    sintaxis basica javascript aprendiendo arrow functiosn

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+var gulp = require('gulp');
+var postcss = require('gulp-postcss')
+var browserSync = require('browser-sync').create()
+
+
+
+
+gulp.task('serv', function () {
+	browserSync.init({
+		server: {
+			baseDir: './dist'
+		}
+	})
+})
+
+// Tarea para procesar el css
+
+gulp.task('css', function(){
+	var processors = [
+		// autoprefixer({browsers : ['> 5%', 'ie 8']}),
+		// atImport(),
+		// mixins(),
+		// cssnested,
+		// lost(),
+		// rucksack(),
+		// cssnext({browsers : ['> 5%', 'ie 8']}),
+		// mqpacker(),
+		// csswring()
+	]
+
+		return gulp.src('./src/styles.css')
+		.pipe(postcss(processors))
+		.pipe(gulp.dest('./dist/css'))
+		.pipe(browserSync.stream())
+
+})
+
+// Tarea para vigilar los cambios 
+
+gulp.task('watch', function(){
+	gulp.watch('./src/*.css', ['css'])
+	gulp.watch('./dist/*.html').on('change', function(){
+		browserSync.reload()
+	})
+
+
+})
+
+
+gulp.task('default', ['watch', 'serv', 'css'])
