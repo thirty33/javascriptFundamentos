d1635906c6082a0311d09fbcc60cda322fca601d
thirty33 <joelsuarez.1101@gmail.com>
2017-08-17 14:18:11 -0500

    aprendiendo uso de bind() para llamadas al this padre, creando clase Toggable()

--- dist/tablero.js
+++ dist/tablero.js
@@ -20,7 +20,89 @@ class Persona {
 		this.amigos.forEach( amigo => { console.log(`soy ${this.nombre} y soy amigo de ${amigo}`)})
 	}
 
+
+
 }
 
-const sacha = new Persona("Sacha", ['pedro', 'maria','joel'])
-console.log(sacha.ListarAmigos())
+class Tablero {
+	constructor(){
+		this.row = new Array(8)
+		this.col = new Array(
+			this.row,
+			this.row,
+			this.row,
+			this.row,
+			this.row,
+			this.row,
+			this.row,
+			this.row,
+		)
+	}
+	agregarvalor(val, posx,posy){
+		this.col[posx][posy] = val
+	}	
+
+	imprimir(x,y) {
+		return this.col[x][y]
+	}
+		
+
+
+
+	
+		
+
+
+
+}
+
+// const sacha = new Persona("Sacha", ['pedro', 'maria','joel'])
+// console.log(sacha.ListarAmigos())
+
+// const tablero = new Tablero()
+// tablero.agregarvalor(3, 0,0)
+
+// console.log(tablero.imprimir(0,0))
+
+
+class Toggable {
+	constructor(el, div, parrafo){
+		this.el = el
+		this.div = div
+		this.parrafo = parrafo
+		this.el.innerHTML = 'Off'
+		this.activated = false
+		this.el.addEventListener('click', this.onClick.bind(this))
+	}
+
+	onClick(){
+		this.activated = !this.activated
+		this.toggleText()
+		this.toggleMenu()
+		this.mostrarParrafo()
+
+	}
+
+	toggleText() {
+		this.el.innerHTML = this.activated ? 'On' : 'Off'
+	}
+
+	toggleMenu(){
+		this.div.style.height = this.activated ? '500px' : '100px'
+
+	}
+	mostrarParrafo(){
+		this.parrafo.style.display = this.activated ? 'block' : 'none'
+	}
+
+}
+
+
+
+
+const button = document.getElementById('boton')
+const menuExtendible = document.getElementById('extender')
+const parrafo = document.getElementById('parrafo')
+
+const miBoton = new Toggable(button,menuExtendible, parrafo)
+
