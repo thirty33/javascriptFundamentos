e4e68d1ad8da8659430114b81efca574bc1658df
thirty33 <joelsuarez.1101@gmail.com>
2017-08-21 08:48:32 -0500

    fibonacci con iteradores en javascript

--- dist/tablero.js
+++ dist/tablero.js
@@ -634,4 +634,32 @@ console.log(`fiboRecursivo en pos 5 es ${fiboRecursivo(5)}`)
 
 
 
+// fibonacci con iteradores en javascript
+
+function fibonacciIterator() {
+	let a=0, b=1
+
+	return {
+		next: function (){
+			let f = a
+			a = b
+			b = f + a 
+			return {value: f, done:false}
+		}
+	}
+}
+
+const fibo = {}
+fibo[Symbol.iterator] = fibonacciIterator
+
+let i = 0
+for(let value of fibo) {
+	console.log('el valor es', value)
+	i++
+	if(i>20) break
+}
+
+
+
+
 
